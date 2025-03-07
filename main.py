1c1bb4aa3821fd9f3fee58f9e904e00fb8877f8b
organizedanvrchy <vimalramnarain@gmail.com>
2025-03-06 23:33:59 -0500

    Added functionality to text box - add user entered task to list

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import streamlit as st
 import functions
 
 todos = functions.get_tasks()
+
+def add_todo():
+  todo = st.session_state["new_todo"]
+  todos.append(todo)
+  if todo:
+    functions.add_tasks(todos)
+
+
 st.title("To-Do App")
 st.subheader("Welcome.")
 st.write("This App increases your productivity.")
@@ -10,7 +18,9 @@ st.write("This App increases your productivity.")
 for todo in todos:
   st.checkbox(todo)
 
-user_input = st.text_input(label="Enter a To-Do", label_visibility="hidden", placeholder="Enter a To-Do")
-if user_input:
-  functions.add_tasks(user_input)
+st.text_input(label="Enter a To-Do", 
+              label_visibility="hidden", 
+              placeholder="Enter a To-Do",
+              on_change=add_todo, key="new_todo")
+
   
